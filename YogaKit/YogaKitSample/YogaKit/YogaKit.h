7ee237711bf40fa5e6fcc26ecfd5687d8ec8d364
David Hart <david@hartbit.com>
2017-01-10 10:31:47 +0100

    carthage support

---
+++ YogaKit/YogaKitSample/YogaKit/YogaKit.h
@@ -0,0 +1,19 @@
+//
+//  YogaKit.h
+//  YogaKit
+//
+//  Created by David Hart on 10.01.17.
+//  Copyright © 2017 facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YogaKit.
+FOUNDATION_EXPORT double YogaKitVersionNumber;
+
+//! Project version string for YogaKit.
+FOUNDATION_EXPORT const unsigned char YogaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YogaKit/PublicHeader.h>
+
+
